6d005a6134818ebc57c7104c354d4780d5b58f0c
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-06-05 09:34:44 -0300

    add-

--- script.js
+++ script.js
@@ -49,3 +49,21 @@ audio.addEventListener('ended', () => {
   currentLine = 0;
   lyricsContainer.scrollTop = 0;
 });
+
+function criarCoracao() {
+    const coracao = document.createElement("div");
+    coracao.innerText = "💗";
+    coracao.style.position = "absolute";
+    coracao.style.left = Math.random() * window.innerWidth + "px";
+    coracao.style.top = Math.random() * window.innerHeight + "px";
+    coracao.style.fontSize = Math.random() * 30 + 20 + "px";
+    coracao.style.opacity = 0.6;
+    coracao.style.animation = "subir 5s linear forwards"; // forwards pra sumir no fim
+    document.body.appendChild(coracao);
+
+    setTimeout(() => coracao.remove(), 5000);
+  }
+
+  setInterval(criarCoracao, 300);
+
+
